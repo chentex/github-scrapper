42da21505a1914bf69feee65cb7b6e30f0379744
Vicente Zepeda Mas <vzepedam@redhat.com>
2023-03-30 12:23:39 +0200

    Added organizaitons file
    Fixed client ussage
    Output json
    
    Signed-off-by: Vicente Zepeda Mas <vzepedam@redhat.com>

--- main.go
+++ main.go
@@ -2,27 +2,57 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/google/go-github/v50/github"
 )
 
-func main() {
-	client := github.NewClient(nil)
+type Repo struct {
+	Name   string   `json:"name"`
+	Topics []string `json:"topics"`
+}
 
-	// list all organizations for user "willnorris"
-	repos, _, err := client.Repositories.List(context.Background(), "cloud-bulldozer", nil)
+func main() {
+	var noTopics []string
+	var repos []Repo
+	b, err := os.ReadFile("organizations")
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
-	for _, r := range repos {
-		tags, _, err := client.Repositories.ListAllTopics(context.Background(), "cloud-bulldozer", *r.Name)
+	orgs := strings.Split(string(b), "\n")
+	client := github.NewClient(nil)
+
+	for _, o := range orgs {
+		ghrepos, _, err := client.Repositories.List(context.Background(), o, nil)
 		if err != nil {
 			fmt.Println(err)
+			os.Exit(1)
 		}
-		fmt.Println(*r.Name)
-		for _, v := range tags {
-			fmt.Printf("\t%s", v)
+		for _, r := range ghrepos {
+			topics := r.Topics
+			if err != nil {
+				fmt.Println(err)
+			}
+			if len(topics) > 0 {
+				repo := Repo{Name: r.GetName(), Topics: topics}
+				repos = append(repos, repo)
+			} else {
+				noTopics = append(noTopics, r.GetName())
+			}
 		}
+	}
+	reposJson, err := json.Marshal(repos)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	os.WriteFile("repos.json", reposJson, os.ModeAppend)
+	fmt.Println("This Repos have no TOPICS")
+	for _, v := range noTopics {
+		fmt.Println(v)
 	}
 }
